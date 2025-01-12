bc3b5383306ca56357612cfc393dc640eb76462e
Marmotte <matthieu.epiard@protonmail.com>
2025-01-12 19:20:05 +0000

    Handle specific sign-in prompt for BlockedTorExitError
    Added a condition to check for sign-in-related error messages before raising a BlockedTorExitError. This ensures more precise error handling and avoids raising the exception unnecessarily in unrelated cases.

--- youtube_dl/YoutubeDL.py
+++ youtube_dl/YoutubeDL.py
@@ -937,7 +937,9 @@ class YoutubeDL(object):
                 self.report_error(msg)
             except ExtractorError as e:  # An error we somewhat expected
                 if self.__tor_proxy_cycle is not None:  # and e.cause.status in BlockedTorExitError.CAUGHT_ERROR_CODES:  # e.cause can be an URLError and thus not having status.
-                    raise BlockedTorExitError(e.cause) from None
+                    error_message = e.orig_msg.lower()
+                    if 'sign in to confirm ' in error_message:
+                        raise BlockedTorExitError(e.cause) from None
                 self.report_error(compat_str(e), tb=e.format_traceback())
             except MaxDownloadsReached:
                 raise
